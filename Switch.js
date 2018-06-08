81dfed93eae504ab68fe480ad44b72a6ea9c2f3d
2534290808 <2534290808@qq.com>
2018-06-08 10:01:56 +0800

    add useNative属性

--- Switch.js
+++ Switch.js
@@ -5,7 +5,7 @@ import {
     Text,
     Platform,
     ViewPropTypes,
-    Switch as SwitchIOS,
+    Switch as SwitchNative,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import SwitchButton from './src/SwitchButton';
@@ -13,16 +13,16 @@ import SwitchButton from './src/SwitchButton';
 class Switch extends Component {
 
     render() {
-        if(Platform.OS==='ios'){
+        if(this.props.useNative){
             return (
-                <SwitchIOS
+                <SwitchNative
                     {...this.props}
                 />
             )
         }else{
             return (
                 <SwitchButton
-                   {...this.props}
+                    {...this.props}
                 />
             )
         }
@@ -35,6 +35,9 @@ Switch.propTypes = {
     onTintColor:PropTypes.string,
     thumbTintColor:PropTypes.string,
     tintColor:PropTypes.string,
+    useNative:PropTypes.bool,//使用RN官方提供的switch
+    tintColorUsage:PropTypes.oneOf(['border','background']),//tintColor的使用方式，在useNative为false时有效
+    useNativeDriver:PropTypes.bool,//是否使用原生动画，在useNative为false时有效
     ...ViewPropTypes,
 }
 export default Switch;
